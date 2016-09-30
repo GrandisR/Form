6105349e49e26c84c5e0bd4af0d18fba12f1c922
Grandis Rufaidah <grandisrufaidah99@gmail.com>
2016-09-30 08:46:44 -0700

    desain tampilan

--- app/src/main/java/id/sch/smktelkom_mlg/learn/form/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/form/MainActivity.java
@@ -2,12 +2,34 @@ package id.sch.smktelkom_mlg.learn.form;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    EditText etNama;
+    EditText etJumlah;
+    Button bOK;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etJumlah = (EditText) findViewById(R.id.editTextJumlah);
+        bOK = (Button) findViewById(R.id.buttonOK);
+
+        bOK.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
     }
 }
